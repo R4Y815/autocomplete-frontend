73c36fdead4654aee20907f493491f21d9ec982d
R4Y815 <Akcent360@gmail.com>
2022-11-09 19:25:48 +0800

    corrected syntax for calling env variable.

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,14 @@
 import React, { useState, useRef, useEffect } from 'react';
-import axios from 'axios';
-import { v4 as uuidv4 } from 'uuid';
+/* import axios from 'axios';
+import { v4 as uuidv4 } from 'uuid'; */
 
 // from component folder
+import { Octokit } from "@octokit/core";
 
 
-const ghToken = process.env.GH_TOKEN;
+// GLOBAL Auth from Octokit for GitHub API
+const ghToken = process.env.REACT_APP_GH_TOKEN;
+
 
 export default function Form() {
     // STATES
@@ -20,45 +23,29 @@ export default function Form() {
         input1Ref.current.value = '';
     }
 
-    /*     // FN: Helper: RETURN DIFFERENCE BETWEEN 2 Sets
-        const difference = (oldSet, newSet) => {
-            if (oldSet.size === 0) {
-                const diffSet = new Set(newSet);
-                for (const elem of oldSet) {
-                    diffSet.delete(elem);
-                }
-                return diffSet
-            }
-            const diffSet = new Set(oldSet);
-            for (const elem of newSet) {
-                diffSet.delete(elem);
-            }
-            return diffSet
-        }
-    
-        // FN: Helper: RETURN DIFFERENCE BETWEEN 2 Arrays
-        const findDiff = (oldArray, newArray) => {
-            const oldArraySet = new Set(oldArray,);
-            const newArraySet = new Set(newArray);
-            const diff = difference(oldArraySet, newArraySet);
-    
-            if (diff.size > 0) {
-                const diffOut = Array.from(diff);
-                return diffOut;
-            }
-            else {
-                return [];
-            }
-        } */
+    // FN: Check Rate Limit
+    /* const checkRate = () => {
+        const octokit = new Octokit({
+            auth: ghToken
+        })
+        octokit.request('GET /rate_limit', {}).then((response) =>
+            console.log(`Limit: ${response.data.rate.limit} | Remain: ${response.data.rate.remaining}`)
+        );
+    } */
 
     // FN: RUN SEARCH for Each letter input:
-    const possibleWords = (keyword) => {
-        axios.get(`https://api.github.com/search/topics?q=${keyword}&per_page=5`, {
-            headers: {
-                'Accept': 'application/vnd.github+json',
-                'Authorization': ghToken
+    const possibleWords = async (keyword) => {
+        /* axios.get(`https://api.github.com/search/topics?q=${keyword}&per_page=5`, {
+            'headers': {
+                'Accept': 'applciation/vnd.github/json',
+                'Authorization': `${ghToken}`
             }
+        }
+        ) */
+        const octokit = new Octokit({
+            auth: ghToken
         })
+        await octokit.request(`GET /search/topics?q=${keyword}&per_page=5`, {})
             .then((results) => {
                 const tempTitles = [];
                 const items = results.data.items;
@@ -68,19 +55,7 @@ export default function Form() {
                     if (x.name.includes(keyword)) { tempTitles.push(x.name) }
                 });
                 setPossibles(tempTitles);
-                /*              // CODE THAT DIDN't REALLY WORK -   
-                                // Compare this array with the old and return the diff:
-                                console.log('tempTitles =', tempTitles);
-                                const newMatches = findDiff(possibles, tempTitles);
-                                console.log('newMatches =', newMatches);
-                                // Combine difference and return diff with old matches:
-                                const newPossibles = [...possibles, ...newMatches];
-                                setPossibles(newPossibles); */
-
-                // RECURRENT PROBLEM:
-                // 'bug-tra' -> correctly displays 5 results
-                // when 'bug-tra' -> 'bug-trac' becomes 10 results... ( repeat 5. )
-                // how about comparing the returned array with the existing state array?
+                /* checkRate(); */
             })
             .catch((error) => {
 
@@ -88,6 +63,7 @@ export default function Form() {
             })
 
     }
+
 
     // FN: Controlled Form Inputs
     const handleInputChange = (event) => {
