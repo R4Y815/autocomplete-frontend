6b3a646accb75ee3bce4c00d769078978da3586e
R4Y815 <Akcent360@gmail.com>
2022-11-10 17:56:46 +0800

    Code CleanUp + Checking InputField before submitting request to backend.

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,39 +1,40 @@
-import React, { useState, useRef, useEffect } from 'react';
+// Modules: React, MUI
+import React, { useState } from 'react';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 import Stack from '@mui/material/Stack';
 import IconButton from '@mui/material/IconButton';
 import SearchButton from '@mui/icons-material/Search';
 
-// from component folder
+// Modules: GitHub 
 import { Octokit } from "@octokit/core";
+
+// Modules: for AJAX out to BE
+import axios from 'axios';
 
 // GLOBAL Auth from Octokit for GitHub API
 const ghToken = process.env.REACT_APP_GH_TOKEN;
 
+// REQUIRED FOR CONNECTION TO BACKEND
+// ensure that axios always sends the cookie to the backend server
+axios.defaults.withCredentials = false;
+
+const REACT_APP_BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:3004';
 
 export default function Form() {
     // STATES
     const [keyword, setKeyword] = useState('');
     const [possibles, setPossibles] = useState([]);
 
-    // REFS
-    const input1Ref = useRef();
-
-    // FN: CLEAR INPUTS
-    const clearInputs = () => {
-        input1Ref.current.value = '';
+    // FN: CLEAR INPUTS IN AUTOCOMPLETE 
+    //     - when clearIcon is clicked
+    const keywordClearOnSelect = () => {
+        const close = document.getElementsByClassName("MuiAutocomplete-clearIndicator")[0];
+        if (close !== undefined) {
+            close.addEventListener("click", () => { setKeyword(''); });
+        }
     }
 
-    // FN: Check Rate Limit 
-    /* const checkRate = () => {
-        const octokit = new Octokit({
-            auth: ghToken
-        })
-        octokit.request('GET /rate_limit', {}).then((response) =>
-            console.log(`Limit: ${response.data.rate.limit} | Remain: ${response.data.rate.remaining}`)
-        );
-    } */
 
     // FN: RUN SEARCH for Each letter input:
     const possibleWords = async (keyword) => {
@@ -78,8 +79,19 @@ export default function Form() {
 
     //FN: Pass Keyword to backend
     const passToBackend = () => {
-        console.log('Button Working');
+        if (keyword !== null) {
+            console.log('Search term sent to backend...');
+            const confirmedTerm = { toSearch: keyword };
+            axios.post(`${REACT_APP_BACKEND_URL}/searchGitHubTopic`, confirmedTerm).then((result) => {
+                console.log('Results of Backend Call to Github Search API =', result);
+            }).catch((error) => {
+                console.log(error);
+            })
+        }
     }
+
+    // Call Clear Inputs function so it Listens in Autocomplete Field
+    keywordClearOnSelect();
 
     return (
         <div
@@ -95,7 +107,6 @@ export default function Form() {
                     freeSolo={true}
                     onChange={handleChosenWord}
                     id="autocomplete-field"
-                    disableClearable
                     options={possibles.map((possible) => possible)}
                     renderInput={(params) => (
                         <TextField
@@ -112,12 +123,12 @@ export default function Form() {
                 />
                 <IconButton
                     onClick={passToBackend}
-                    color="primary"
+                    color={keyword.length > 1 ? "primary" : "error"}
                     aria-label="search Github Topics"
                     size="large">
                     <SearchButton fontSize='large' />
                 </IconButton>
             </Stack>
-        </div>
+        </div >
     )
 }
