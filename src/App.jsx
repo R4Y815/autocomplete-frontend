e1b0bdee97af2b0a130cce643378a310bab17fcc
R4Y815 <Akcent360@gmail.com>
2022-11-10 17:58:11 +0800

    Code Cleanup.

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,10 @@
 import './App.css';
-
-import React, { useState } from 'react';
-import axios from 'axios';
+import React from 'react';
 
 // import Fn components from src/compoenents
 import Form from './components/Form.jsx'
 
-// ensure that axios always sends the cookie to the backend server
-axios.defaults.withCredentials = false;
-
-const REACT_APP_BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:3004';
-
 export default function App() {
-
-    // write your frontend facing REACT paradigm code here
-
-
-
-
-
-    // 
-
 
     return (
         <div>
